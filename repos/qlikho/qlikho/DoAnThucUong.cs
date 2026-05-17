eb0f15c27c106d9a18da59aa9682ce0a813be2aa
Nlinh <kimnhatlinh541@gmail.com>
2026-05-17 09:24:01 +0700

    Add files via upload

---
+++ repos/qlikho/qlikho/DoAnThucUong.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace qlikho
+{
+    public class DoAnThucUong
+    {
+        public string maDoAn { get; set; }
+        public string tenDoAn { get; set; }
+        public string loaiDoAn { get; set; }
+        public decimal giaTien { get; set; }
+        public int soLuongTon { get; set; }
+    }
+}
